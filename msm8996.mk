6d9a9fe806b8ab288d4abbc896a1c9eb053ff593
pranav <pranavtemkar@gmail.com>
2020-07-01 09:09:27 +0000

    msm8996-common: prebuilts: Add Google Camera Go Apk kanged from Parrot043 1.0.289787409 | V2

--- msm8996.mk
+++ msm8996.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
+
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
